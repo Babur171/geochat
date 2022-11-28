d129505cb8ee261f990c22377c71ae1df8b174e6
Babur171 <baburali894@gmail.com>
2022-11-29 00:53:51 +0500

    fixed new issues

--- src/Navigators/AuthScreens.js
+++ src/Navigators/AuthScreens.js
@@ -1,91 +1,96 @@
-import React from "react"
-import { createStackNavigator } from "@react-navigation/stack"
-import { Image, Dimensions, View, Text } from "react-native"
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Image, Dimensions, View, Text } from "react-native";
 // screens
-import Welcome from '@/Screens/Welcome'
-import { SignUp } from '@/Screens/SignUp'
-import { SignIn } from '@/Screens/SignIn'
-import { Language } from '@/Screens/Language'
-import { OTP } from '@/Screens/Otp'
-import GetStart from '@/Screens/GetStart'
-import {
-  ChattingList,
-} from "@/Screens/ChattingList"
-import {ChooseImage} from '@/Screens/ChooseImage'
-import { ChatHistory } from '@/Screens/ChatHistory'
-import { ChatInfo } from '@/Screens/ChatInfo'
+import Welcome from "@/Screens/Welcome";
+import { SignUp } from "@/Screens/SignUp";
+import { SignIn } from "@/Screens/SignIn";
+import { Language } from "@/Screens/Language";
+import { OTP } from "@/Screens/Otp";
+import GetStart from "@/Screens/GetStart";
+import { ChattingList } from "@/Screens/ChattingList";
+import { ChooseImage } from "@/Screens/ChooseImage";
+import { ChatHistory } from "@/Screens/ChatHistory";
+import { ChatInfo } from "@/Screens/ChatInfo";
+import Security from  "@/Screens/Security";
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-const { width } = Dimensions.get('window')
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const { width } = Dimensions.get("window");
 
-const authStack = createStackNavigator()
-const Tab = createBottomTabNavigator()
+const authStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AuthStackScreen = () => (
   <authStack.Navigator
     screenOptions={{ headerShown: false }}
     initialRouteName="Welcome"
   >
-     <authStack.Screen
-            name="Main"
-            component={Welcome}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          <authStack.Screen
-            name="GetStart"
-            component={GetStart}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          <authStack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          <authStack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          <authStack.Screen
-            name="Language"
-            component={Language}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          <authStack.Screen
-            name="OTP"
-            component={OTP}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-          
-           <authStack.Screen
-            name="BottomNavigator"
-            component={BottomNavigator}
-            options={{
-              animationEnabled: false,
-            }}
-          />
+    <authStack.Screen
+      name="Main"
+      component={Welcome}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+    <authStack.Screen
+      name="GetStart"
+      component={GetStart}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+    <authStack.Screen
+      name="SignUp"
+      component={SignUp}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+    <authStack.Screen
+      name="SignIn"
+      component={SignIn}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+    <authStack.Screen
+      name="Language"
+      component={Language}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+    <authStack.Screen
+      name="OTP"
+      component={OTP}
+      options={{
+        animationEnabled: false,
+      }}
+    />
+      <authStack.Screen
+      name="Security"
+      component={Security}
+      options={{
+        animationEnabled: false,
+      }}
+    />
 
+    <authStack.Screen
+      name="BottomNavigator"
+      component={BottomNavigator}
+      options={{
+        animationEnabled: false,
+      }}
+    />
   </authStack.Navigator>
-)
+);
 
 const BottomNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="ChattingList"
       screenOptions={{
-        tabBarStyle: { height: width * 0.15, paddingBottom: 5 }
+        tabBarStyle: { height: width * 0.15, paddingBottom: 5 },
       }}
     >
       <Tab.Screen
@@ -93,20 +98,21 @@ const BottomNavigator = () => {
         component={ChooseImage}
         options={{
           tabBarLabel: () => (
-            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: '400' }}>Chats</Text>
+            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: "400" }}>
+              Chats
+            </Text>
           ),
           tabBarIcon: ({ focused }) => (
             <Image
               source={
-                focused ? require(
-                  "../Assets/messageFocus.png")
-                  : require(
-                    "../Assets/message.png")
+                focused
+                  ? require("../Assets/messageFocus.png")
+                  : require("../Assets/message.png")
               }
-              style={{ height: 34, width: 31, }}
+              style={{ height: 34, width: 31 }}
             />
           ),
-          header: () => null
+          header: () => null,
         }}
       />
       <Tab.Screen
@@ -114,20 +120,21 @@ const BottomNavigator = () => {
         component={ChattingList}
         options={{
           tabBarLabel: () => (
-            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: '400' }}>Contacts</Text>
+            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: "400" }}>
+              Contacts
+            </Text>
           ),
           tabBarIcon: ({ focused }) => (
             <Image
               source={
-                focused ? require(
-                  "../Assets/adduserFocus.png")
-                  : require(
-                    "../Assets/adduser.png")
+                focused
+                  ? require("../Assets/adduserFocus.png")
+                  : require("../Assets/adduser.png")
               }
-              style={{ height: 24, width: 39, }}
+              style={{ height: 24, width: 39 }}
             />
           ),
-          header: () => null
+          header: () => null,
         }}
       />
       <Tab.Screen
@@ -135,20 +142,21 @@ const BottomNavigator = () => {
         component={ChatHistory}
         options={{
           tabBarLabel: () => (
-            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: '400' }}>Discover</Text>
+            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: "400" }}>
+              Discover
+            </Text>
           ),
           tabBarIcon: ({ focused }) => (
             <Image
-            source={
-              focused ? require(
-                "../Assets/globeFocus.png")
-                : require(
-                  "../Assets/globe.png")
-            }
-              style={{ height: 25, width: 22, }}
+              source={
+                focused
+                  ? require("../Assets/globeFocus.png")
+                  : require("../Assets/globe.png")
+              }
+              style={{ height: 25, width: 22 }}
             />
           ),
-          header: () => null
+          header: () => null,
         }}
       />
       <Tab.Screen
@@ -156,23 +164,24 @@ const BottomNavigator = () => {
         component={ChatInfo}
         options={{
           tabBarLabel: () => (
-            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: '400' }}>Me</Text>
+            <Text style={{ fontSize: 12, lineHeight: 15, fontWeight: "400" }}>
+              Me
+            </Text>
           ),
           tabBarIcon: ({ focused }) => (
             <Image
-            source={
-              focused ? require(
-                "../Assets/profileFocus.png")
-                : require(
-                  "../Assets/profile.png")
-            }
-              style={{ height: 27, width: 24, }}
+              source={
+                focused
+                  ? require("../Assets/profileFocus.png")
+                  : require("../Assets/profile.png")
+              }
+              style={{ height: 27, width: 24 }}
             />
           ),
-          header: () => null
+          header: () => null,
         }}
       />
     </Tab.Navigator>
-  )
-}
-export default AuthStackScreen
+  );
+};
+export default AuthStackScreen;
---
+++ src/Screens/Security/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+    Text,
+    View,
+    Image,
+    ImageBackground,
+    TouchableOpacity
+} from "react-native";
+import { styles } from "./style";
+import { Button } from "@/Components/Button";
+
+const Security = ({ navigation }) => {
+
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require("../../Assets/securityBg.png")} resizeMode="cover" style={styles.blurImageStyle}>
+                <View style={styles.titleContainer}>
+                    <TouchableOpacity>
+                    <Image
+                        // @ts-ignore
+                        source={require("../../Assets/cross.png")}
+                        style={[styles.logo,{width:22,height:22,resizeMode:'contain',marginLeft:29,marginTop:69}]}
+                    />
+                    </TouchableOpacity>
+                    <View style={{flexDirection:'row',marginTop:108}}>
+                    <Image
+                        // @ts-ignore
+                        source={require("../../Assets/not.png")}
+                        style={[styles.logo,{width:40,height:40,resizeMode:'contain',marginLeft:31}]}
+                    />
+                    <Text style={[styles.description,{marginLeft:22,fontSize:23,fontWeight:"700",color:'black'}]}>
+                        Security Verification
+                    </Text>
+                    </View>
+                    <View style={{marginHorizontal:59,marginTop:26}}>
+                    <Text style={{fontSize:13,fontWeight:"400",lineHeight:16}}>
+                    For the security of your account, verify security verification code before 
+                    </Text>
+                    </View>
+                </View>
+
+                {/* <View style={styles.buttonContainer}>
+                    <Button
+                        buttonText="Lets Start"
+                        style={styles.button}
+                        backgroundColor="#4285F4"
+                        textColor="#000"
+                        hideShadow
+                        onPress={() => navigation.navigate("SignIn")}
+                    />
+                </View> */}
+            </ImageBackground>
+
+        </View>
+    );
+};
+
+
+export default Security;
+
